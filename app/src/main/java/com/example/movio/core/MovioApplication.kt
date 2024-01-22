f5b536bb7aba83e3eb15ead9e6ed17faadc17884
karim <karim_agha5@hotmail.com>
2024-01-22 22:12:44 +0200

    refactor: added getAppContext() static function inside the MovioApplication; add: MessageShower utility class to show dialogs and loading inside fragments in the authentication flow (experimental)

--- app/src/main/java/com/example/movio/core/MovioApplication.kt
+++ app/src/main/java/com/example/movio/core/MovioApplication.kt
@@ -1,14 +1,19 @@
 package com.example.movio.core
 
 import android.app.Application
+import android.content.Context
 import com.google.firebase.FirebaseApp
 
 class MovioApplication : Application() {
 
+    companion object{
+        lateinit var appContext: Context
+    }
     lateinit var movioContainer: MovioContainer
     override fun onCreate() {
         super.onCreate()
         movioContainer = MovioContainer(this)
         FirebaseApp.initializeApp(this)
+        appContext = applicationContext
     }
 }
---
+++ app/src/main/java/com/example/movio/feature/common/helpers/MessageShower.kt
@@ -0,0 +1,49 @@
+package com.example.movio.feature.common.helpers
+
+import android.provider.Settings.Global.getString
+import androidx.core.content.res.ResourcesCompat
+import com.example.movio.R
+import com.example.movio.core.MovioApplication
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.progressindicator.CircularProgressIndicatorSpec
+import com.google.android.material.progressindicator.IndeterminateDrawable
+import java.lang.ref.WeakReference
+
+object MessageShower {
+    private lateinit var weakProgressIndicator: WeakReference<IndeterminateDrawable<CircularProgressIndicatorSpec>>
+    init {
+        val spec = CircularProgressIndicatorSpec(
+            MovioApplication.appContext,
+            null,
+            0,
+            com.google.android.material.R.style.Widget_Material3_CircularProgressIndicator_ExtraSmall
+        )
+        weakProgressIndicator = WeakReference(IndeterminateDrawable.createCircularDrawable(MovioApplication.appContext, spec))
+    }
+
+     fun startGoogleAuthenticationLoading(btn: MaterialButton,strRes: Int){
+         btn.icon = weakProgressIndicator.get()
+         btn.iconGravity = MaterialButton.ICON_GRAVITY_TEXT_START
+         btn.text = MovioApplication.appContext.getText(strRes)
+    }
+
+     fun startTwitterAuthenticationLoading(btn: MaterialButton,strRes: Int){
+         btn.icon = weakProgressIndicator.get()
+         btn.iconGravity = MaterialButton.ICON_GRAVITY_TEXT_START
+         btn.text = MovioApplication.appContext.getText(strRes)
+    }
+
+     fun stopGoogleAuthenticationLoading(btn: MaterialButton,strRes: Int){
+        btn.icon = ResourcesCompat.getDrawable(MovioApplication.appContext.resources,
+            R.drawable.google_circular_icon,MovioApplication.appContext.theme)
+        btn.iconGravity = MaterialButton.ICON_GRAVITY_TEXT_START
+        btn.text = MovioApplication.appContext.getText(strRes)
+    }
+
+     fun stopTwitterAuthenticationLoading(btn: MaterialButton,strRes: Int){
+        btn.icon = ResourcesCompat.getDrawable(MovioApplication.appContext.resources,
+            R.drawable.twitter_icon,MovioApplication.appContext.theme)
+        btn.iconGravity = MaterialButton.ICON_GRAVITY_TEXT_START
+        btn.text = MovioApplication.appContext.getText(strRes)
+    }
+}
