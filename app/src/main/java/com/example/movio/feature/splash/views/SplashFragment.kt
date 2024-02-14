7925a5d252f49cf778c6a7e1393cbfba63570f7c
karim <karim_agha5@hotmail.com>
2024-02-14 22:04:27 +0200

    chore: SplashFragment cleanup and write documentation

--- app/src/main/java/com/example/movio/feature/splash/views/SplashFragment.kt
+++ app/src/main/java/com/example/movio/feature/splash/views/SplashFragment.kt
@@ -4,42 +4,29 @@ import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
+import com.example.movio.MainActivity
 import com.example.movio.core.common.BaseFragment
 import com.example.movio.core.common.BaseViewModel
 import com.example.movio.core.helpers.ViewModelDelegate
-import com.example.movio.core.navigation.CoordinatorHost
+import com.example.movio.core.navigation.RootCoordinator
 import com.example.movio.databinding.FragmentSplashBinding
 import com.example.movio.feature.common.actions.AuthenticationActions
 import com.example.movio.feature.common.status.UserAuthenticationStatus
+import com.example.movio.feature.splash.viewmodel.SplashViewModel
 
 class SplashFragment : BaseFragment<FragmentSplashBinding>(),LifecycleEventObserver {
-
-   /*private val splashViewModel by lazy {
-       movioApplication
-           .movioContainer
-           .rootCoordinator
-           .requireViewModel<Nothing,AuthenticationActions,Nothing>(this::class.java)
-   }*/
-
-
 
     private lateinit var splashViewModel: BaseViewModel<Nothing,AuthenticationActions, UserAuthenticationStatus>
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         activity?.lifecycle?.addObserver(this)
-    }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.i("MainActivity", "onCreate in fragment : ")
-
     }
 
     override fun inflateBinding(
@@ -63,6 +50,13 @@ class SplashFragment : BaseFragment<FragmentSplashBinding>(),LifecycleEventObser
         splashViewModel.postAction(null,AuthenticationActions.ToSplashScreen)
     }
 
+    /**
+     * Necessary for the [SplashViewModel] instantiation.
+     * The instantiation of the [SplashViewModel] requires the [MainActivity] onCreate() lifecycle
+     * callback to be called first so that it initializes the [RootCoordinator] state correctly.
+     * Therefore, an observation on the [MainActivity] lifecycle is necessary.
+     * The observation on the [MainActivity] lifecycle is registered in the [onAttach] lifecycle callback.
+    * */
     override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
         if (event == Lifecycle.Event.ON_CREATE) {
             val vm by ViewModelDelegate<Nothing, AuthenticationActions, UserAuthenticationStatus>(
