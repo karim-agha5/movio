25fdeb44a245899642c4fa79789af02f4e00be3f
karim <karim_agha5@hotmail.com>
2024-02-16 15:02:18 +0200

    chore: cleaning up SignupFragment comments and log messages

--- app/src/main/java/com/example/movio/feature/authentication/signup/views/SignupFragment.kt
+++ app/src/main/java/com/example/movio/feature/authentication/signup/views/SignupFragment.kt
@@ -47,30 +47,10 @@ class SignupFragment :
     AuthenticationResultCallbackLauncher,
     LifecycleEventObserver {
 
-
-    private val coordinator by lazy {
-        movioApplication
-            .movioContainer
-            .rootCoordinator
-            .requireCoordinator()
-    }
-    //private val authenticationHelper by lazy { movioApplication.movioContainer.authenticationHelper }
-    /*
-   *  TODO find another way to get a view model other than the coordinator.
-   *   the coordinator is supposed to be only inside the view model.
-   *   the view should be agnostic of anything except its state
-   * */
-    /*private val signupViewModel by lazy {
-        coordinator
-            .requireViewModel<SignupCredentials, SignupActions, Event<SignupStatus>>(this::class.java)
-        as FederatedAuthenticationBaseViewModel
-    }*/
     private lateinit var signupViewModel: FederatedAuthenticationBaseViewModel<SignupCredentials, SignupActions, Event<SignupStatus>>
-    private lateinit var googleSignInService: GoogleSignInService
     private lateinit var authenticationLifecycleObserver: AuthenticationLifecycleObserver
     private lateinit var progressIndicatorDrawable: IndeterminateDrawable<CircularProgressIndicatorSpec>
     private lateinit var credentialsProgressIndicatorDrawable: IndeterminateDrawable<CircularProgressIndicatorSpec>
-    //private lateinit var disposable: Disposable
 
 
     override fun onAttach(context: Context) {
@@ -80,11 +60,6 @@ class SignupFragment :
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //val vm by ViewModelDelegate<SignupCredentials, SignupActions, Event<SignupStatus>>(movioApplication,this::class.java)
-        //signupViewModel = vm as FederatedAuthenticationBaseViewModel<SignupCredentials, SignupActions, Event<SignupStatus>>
-        //googleSignInService = GoogleSignInService.getInstance(requireActivity(),this)
-        //signupViewModel.register(requireActivity())
-        //signupViewModel.register(this)
         authenticationLifecycleObserver =
             AuthenticationLifecycleObserver(
                 this::class.java.simpleName,
@@ -99,8 +74,6 @@ class SignupFragment :
 
     override fun onResume() {
         super.onResume()
-        Log.i("MainActivity", "onResume: ")
-        //signupViewModel.register(requireActivity())
         signupViewModel.register(this)
     }
 
@@ -111,23 +84,23 @@ class SignupFragment :
         return FragmentSignupBinding.inflate(inflater,container,false)
     }
 
+    /**
+     * Necessary for the [SignupViewModel] instantiation.
+     * The instantiation of the [SignupViewModel] requires the [MainActivity] onCreate() lifecycle
+     * callback to be called first so that it initializes the [RootCoordinator] state correctly.
+     * Therefore, an observation on the [MainActivity] lifecycle is necessary.
+     * The observation on the [MainActivity] lifecycle is registered in the [onAttach] lifecycle callback.
+     * */
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        if(event == Lifecycle.Event.ON_CREATE){
+            val vm by ViewModelDelegate<SignupCredentials, SignupActions, Event<SignupStatus>>(movioApplication,this::class.java)
+            signupViewModel = vm as FederatedAuthenticationBaseViewModel<SignupCredentials, SignupActions, Event<SignupStatus>>
+            signupViewModel.register(requireActivity())
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-/*
-        binding.btnFacebook.setOnClickListener {/*TODO implement when the app is published*/}
-        binding.btnGoogle.setOnClickListener { lifecycleScope.launch { startGoogleAuthenticationFlow() } }
-        binding.btnTwitter.setOnClickListener { lifecycleScope.launch { startTwitterAuthenticationFlow() } }
-        binding.tvSignIn.setOnClickListener { navigateToSignInScreen() }
-        binding.btnSignup.setOnClickListener {
-            if(areFieldsValid()){
-                Utils.hideKeyboard(requireActivity())
-                lifecycleScope.launch { signUpUsingEmailAndPassword() }
-            }
-            else{
-                setTextInputLayoutErrorStyling()
-            }
-        }
-*/
 
         binding.btnFacebook.setOnClickListener {/*TODO implement when the app is published*/}
         binding.btnGoogle.setOnClickListener {
@@ -153,18 +126,10 @@ class SignupFragment :
         signupViewModel.result.observe(viewLifecycleOwner){
             it.getContentIfNotHandled()?.let{ status -> onResultReceived(status)}
         }
-
-/*
-        val source = authenticationHelper.getAuthenticationResultObservableSource()
-        disposable = source.subscribe{ onAuthenticationResultReceived(it) }
- */
-
     }
 
     override fun launchAuthenticationResultCallbackLauncher(intentSenderRequest: IntentSenderRequest) {
-        Log.i("MainActivity", "inside launchAuthenticationResultCallbackLauncher")
         authenticationLifecycleObserver.launchAuthenticationResultCallbackLauncher(intentSenderRequest)
-        //(requireActivity() as AuthenticationResultCallbackLauncher).launchAuthenticationResultCallbackLauncher(intentSenderRequest)
     }
 
     private fun onResultReceived(signupStatus: SignupStatus){
@@ -227,140 +192,14 @@ class SignupFragment :
     }
 
     private fun onSignUpFailure(throwable: Throwable?){
-        Log.i("MainActivity", "Inside onSignUpFailure -> ${throwable?.message}\n\n ${throwable?.toString()}")
         throwable?.printStackTrace()
-        //showAppropriateDialog(throwable)
         @OptIn(Experimental::class)
         MessageShower.showAppropriateErrorDialog(requireContext(),throwable)
         stopGoogleAuthenticationLoading()
         stopTwitterAuthenticationLoading()
         stopCredentialsAuthenticationLoading()
     }
-/*
-    private suspend fun startGoogleAuthenticationFlow(){
-        googleSignInService.login(null)
-    }
 
-    private suspend fun startTwitterAuthenticationFlow(){
-        TwitterAuthenticationService
-            .getInstance(
-                requireActivity(),
-                movioApplication.movioContainer.firebaseAuth,
-                AuthenticationHelper
-            )
-            .login(null)
-    }
-
-    private fun signUpUsingEmailAndPassword(){
-        val credentials =
-            SignupCredentials(binding.etEmail.text.toString(),binding.etPassword.text.toString())
-        signupViewModel.postAction(credentials, SignupActions.SignupClicked)
-    }
-*/
-    private fun onEmailVerificationStatusReceived(signInStatus: SignInStatus){
-        when(signInStatus){
-            is SignInStatus.ShouldVerifyEmail -> showShouldVerifyEmailToast()
-            else -> {/* Do Nothing */}
-        }
-    }
-
-    /*private fun onAuthenticationResultReceived(authenticationResult: AuthenticationResult){
-        *//**
-         * TODO should handle the cases were null is sent from the source observable.
-         *  Look [TwitterAuthenticationService]
-         *//*
-        when(authenticationResult){
-            is AuthenticationResult.Success -> {
-                // onSuccessfulAuthentication(authenticationResult.user)
-            }
-            is AuthenticationResult.Failure -> showAppropriateDialog(authenticationResult.throwable)
-        }
-    }*/
-
-   /* private fun onSuccessfulAuthentication(firebaseUser: FirebaseUser?){
-        authenticateUser(firebaseUser)
-        navigateToHome()
-    }
-
-    private fun authenticateUser(firebaseUser: FirebaseUser?){
-        val userManager = UserManager.getInstance(movioApplication.movioContainer.firebaseAuth)
-        userManager.authenticateUser(firebaseUser)
-    }
-
-    private fun navigateToHome(){
-        lifecycleScope.launch { coordinator.postAction(AuthenticationActions.ToHomeScreen) }
-    }
-
-    private fun navigateToSignInScreen(){
-        lifecycleScope.launch { coordinator.postAction(AuthenticationActions.ToSignInScreen) }
-    }
-*/
-
-       /* private fun showAppropriateDialog(throwable: Throwable?) =
-       when (throwable) {
-           is ApiException                          -> {
-               // showDialog(throwable.statusCode)
-               @OptIn(Experimental::class)
-               MessageShower.showAppropriateErrorDialog(requireContext(),throwable)
-           }
-           is FirebaseAuthUserCollisionException    -> {
-               //showDialog(throwable)
-               @OptIn(Experimental::class)
-               MessageShower.showAppropriateErrorDialog(requireContext(),throwable)
-           }
-           else                                     -> {
-               //showDialog(throwable?.message)
-               @OptIn(Experimental::class)
-               MessageShower.showAppropriateErrorDialog(requireContext(),throwable)
-           }
-       }*/
-/*
-    private fun showDialog(message: String?){
-        lifecycleScope.launch(Dispatchers.Main){
-            buildDialog(
-                getString(R.string.generic_authentication_error_title),
-                message
-            ).show()
-        }
-    }
-
-    private fun showDialog(statusCode: Int){
-        val title: String
-        val message: String
-        when(statusCode) {
-            CommonStatusCodes.CANCELED  -> {
-                title = getString(R.string.canceled_authentication_title)
-                message = getString(R.string.canceled_authentication_message)
-            }
-            CommonStatusCodes.NETWORK_ERROR -> {
-                title = getString(R.string.canceled_authentication_title)
-                message = getString(R.string.canceled_authentication_message)
-            }
-            else -> {
-                title = getString(R.string.generic_authentication_error_title)
-                message = getString(R.string.generic_authentication_error_message)
-            }
-        }
-        // Show the dialog on the main thread
-        // because this function is called from an observer on a background thread
-        lifecycleScope.launch(Dispatchers.Main){ buildDialog(title,message).show() }
-    }
-
-    private fun showDialog(throwable: Throwable?){
-        lifecycleScope.launch(Dispatchers.Main) {
-            //buildDialog("Error","This user has already signed up before").show()
-            buildDialog("Error",throwable?.message).show()
-        }
-    }
-
-    private fun buildDialog(title: String,message: String?) : MaterialAlertDialogBuilder {
-        val defaultMessage = getString(R.string.generic_authentication_error_message)
-        return MaterialAlertDialogBuilder(requireContext())
-            .setTitle(title)
-            .setMessage(message ?: defaultMessage)
-            .setNeutralButton(getString(R.string.ok)) { _, _ -> /* Do nothing*/ }
-    }
-*/
     private fun areFieldsValid() : Boolean{
         return isEmailFieldValid() && isPasswordFieldValid()
     }
@@ -407,35 +246,7 @@ class SignupFragment :
 
     }
 
-    private fun setTextInputLayoutErrorStyling(){
-        setEmailFieldStyling()
-        setPasswordFieldStyling()
-    }
-
     private fun showShouldVerifyEmailToast(){
         Toast.makeText(requireContext(), resources.getString(R.string.should_verify_email), Toast.LENGTH_LONG).show()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        //AuthenticationHelper.disposeAuthenticationResult(disposable)
-        // TODO maybe change the state when the user is actually authenticated, not when the fragment is destroyed
-        //lifecycleScope.launch { coordinator.postAction(StateActions.ToAuthenticated) }
-        //signupViewModel.unregister()
-    }
-
-    /**
-     * Necessary for the [SignupViewModel] instantiation.
-     * The instantiation of the [SignupViewModel] requires the [MainActivity] onCreate() lifecycle
-     * callback to be called first so that it initializes the [RootCoordinator] state correctly.
-     * Therefore, an observation on the [MainActivity] lifecycle is necessary.
-     * The observation on the [MainActivity] lifecycle is registered in the [onAttach] lifecycle callback.
-     * */
-    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
-        if(event == Lifecycle.Event.ON_CREATE){
-            val vm by ViewModelDelegate<SignupCredentials, SignupActions, Event<SignupStatus>>(movioApplication,this::class.java)
-            signupViewModel = vm as FederatedAuthenticationBaseViewModel<SignupCredentials, SignupActions, Event<SignupStatus>>
-            signupViewModel.register(requireActivity())
-        }
     }
 }
