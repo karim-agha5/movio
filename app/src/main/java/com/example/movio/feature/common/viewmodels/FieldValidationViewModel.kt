fa5fbc108b911f6552968aceee665db71c8c7474
karim <karim_agha5@hotmail.com>
2024-02-17 14:28:17 +0200

    refactor: FieldValidationViewModel now exposes a StateFlow for the email field state to the UI view instead of a MutaleStateFlow

--- app/src/main/java/com/example/movio/feature/common/viewmodels/FieldValidationViewModel.kt
+++ app/src/main/java/com/example/movio/feature/common/viewmodels/FieldValidationViewModel.kt
@@ -5,6 +5,7 @@ import com.example.movio.feature.common.status.ValidationResultState
 import com.example.movio.feature.common.use_cases.ValidateEmail
 import com.example.movio.feature.common.use_cases.ValidatePassword
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class FieldValidationViewModel(
     private val validateEmail: ValidateEmail,
@@ -12,7 +13,7 @@ class FieldValidationViewModel(
 ) : ViewModel(){
 
     private val _emailFieldState = MutableStateFlow<ValidationResultState>(ValidationResultState.Neutral)
-    val emailFieldsState = _emailFieldState
+    val emailFieldsState = _emailFieldState.asStateFlow()
 
     fun validate(email: String){
         _emailFieldState.value = validateEmail.execute(email)
