e9e04b4701e887bd2dba4584a546fdff6a8775f6
karim <karim_agha5@hotmail.com>
2024-02-17 14:24:14 +0200

    feat: add ValdiatePassword use case to both FieldValidationViewModelFactory and FieldValidationViewModel

--- app/src/main/java/com/example/movio/feature/common/viewmodels/FieldValidationViewModel.kt
+++ app/src/main/java/com/example/movio/feature/common/viewmodels/FieldValidationViewModel.kt
@@ -3,10 +3,12 @@ package com.example.movio.feature.common.viewmodels
 import androidx.lifecycle.ViewModel
 import com.example.movio.feature.common.status.ValidationResultState
 import com.example.movio.feature.common.use_cases.ValidateEmail
+import com.example.movio.feature.common.use_cases.ValidatePassword
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class FieldValidationViewModel(
-    private val validateEmail: ValidateEmail
+    private val validateEmail: ValidateEmail,
+    private val validatePassword: ValidatePassword
 ) : ViewModel(){
 
     private val _fieldsState = MutableStateFlow<ValidationResultState>(ValidationResultState.Neutral)
--- app/src/main/java/com/example/movio/feature/common/viewmodels/FieldValidationViewModelFactory.kt
+++ app/src/main/java/com/example/movio/feature/common/viewmodels/FieldValidationViewModelFactory.kt
@@ -3,13 +3,16 @@ package com.example.movio.feature.common.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.movio.feature.common.use_cases.ValidateEmail
+import com.example.movio.feature.common.use_cases.ValidatePassword
 
 class FieldValidationViewModelFactory (
-    private val validateEmail: ValidateEmail
+    private val validateEmail: ValidateEmail,
+    private val validatePassword: ValidatePassword
 ): ViewModelProvider.Factory{
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return FieldValidationViewModel(
-            validateEmail
+            validateEmail,
+            validatePassword
         ) as T
     }
 }
