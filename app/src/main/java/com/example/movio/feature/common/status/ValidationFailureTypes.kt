ffd455a7f36b4fc0338fb1893062f3a4c913a32e
karim <karim_agha5@hotmail.com>
2024-02-17 14:12:27 +0200

    feat: add password failure types in ValidationFailureTypes

--- app/src/main/java/com/example/movio/feature/common/status/ValidationFailureTypes.kt
+++ app/src/main/java/com/example/movio/feature/common/status/ValidationFailureTypes.kt
@@ -2,5 +2,7 @@ package com.example.movio.feature.common.status
 
 enum class ValidationFailureTypes(val errorMessage: String) {
     EMAIL_FIELD_EMPTY("Please fill the email field"),
-    EMAIL_PATTERN_MISMATCH("The email format is incorrect")
+    EMAIL_PATTERN_MISMATCH("The email format is incorrect"),
+    PASSWORD_FIELD_EMPTY("Please fill the password field"),
+    PASSWORD_LENGTH("The password must be 6 characters or more")
 }
