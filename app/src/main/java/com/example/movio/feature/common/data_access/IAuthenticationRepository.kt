f7e1fd8e7a7c8f6e930f41bb8c9260a63b2a0b2a
karim <karim_agha5@hotmail.com>
2024-02-18 21:38:05 +0200

    refactor: IAuthenticationRepository login and signup methods now return a nullable FirebaseUser

--- app/src/main/java/com/example/movio/feature/common/data_access/IAuthenticationRepository.kt
+++ app/src/main/java/com/example/movio/feature/common/data_access/IAuthenticationRepository.kt
@@ -5,11 +5,12 @@ import com.example.movio.feature.authentication.helpers.AuthenticationResultCall
 import com.example.movio.feature.authentication.services.GoogleSignInService
 import com.example.movio.feature.common.models.LoginCredentials
 import com.example.movio.feature.common.models.SignupCredentials
+import com.google.firebase.auth.FirebaseUser
 
 interface IAuthenticationRepository {
-    suspend fun signup(credentials: SignupCredentials)
+    suspend fun signup(credentials: SignupCredentials) : FirebaseUser?
 
-    suspend fun login(credentials: LoginCredentials)
+    suspend fun login(credentials: LoginCredentials) : FirebaseUser?
 
     suspend fun signupWithGoogle()
 
