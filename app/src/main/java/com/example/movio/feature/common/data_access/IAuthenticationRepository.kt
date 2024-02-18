c6fb0529751f428571113d5f43523b3bdd2393b9
karim <karim_agha5@hotmail.com>
2024-02-18 21:30:44 +0200

    feat: add register and unregister methods to IAuthenticationRepository

--- app/src/main/java/com/example/movio/feature/common/data_access/IAuthenticationRepository.kt
+++ app/src/main/java/com/example/movio/feature/common/data_access/IAuthenticationRepository.kt
@@ -1,5 +1,8 @@
 package com.example.movio.feature.common.data_access
 
+import androidx.activity.ComponentActivity
+import com.example.movio.feature.authentication.helpers.AuthenticationResultCallbackLauncher
+import com.example.movio.feature.authentication.services.GoogleSignInService
 import com.example.movio.feature.common.models.LoginCredentials
 import com.example.movio.feature.common.models.SignupCredentials
 
@@ -9,6 +12,14 @@ interface IAuthenticationRepository {
     suspend fun login(credentials: LoginCredentials)
 
     suspend fun signupWithGoogle()
+
+    fun register(launcher: AuthenticationResultCallbackLauncher)
+
+    fun register(componentActivity: ComponentActivity)
+
+    fun unregister()
+
+    fun getGoogleSignInService() : GoogleSignInService
 
     suspend fun signupWithTwitter()
 
