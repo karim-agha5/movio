0b944d1e87b0c7023f1d708c3293169e910be831
karim <karim_agha5@hotmail.com>
2024-02-28 18:44:57 +0200

    chore: add ValidationFailureTypes documentation

--- app/src/main/java/com/example/movio/feature/common/status/ValidationFailureTypes.kt
+++ app/src/main/java/com/example/movio/feature/common/status/ValidationFailureTypes.kt
@@ -1,5 +1,6 @@
 package com.example.movio.feature.common.status
 
+// TODO consider moving those hardcoded strings to strings.xml
 enum class ValidationFailureTypes(val errorMessage: String) {
     EMAIL_FIELD_EMPTY("Please fill the email field"),
     EMAIL_PATTERN_MISMATCH("The email format is incorrect"),
