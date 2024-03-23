6e941e6ff3f1dbe30e8e1bcd43dd70bb655437b4
karim <karim_agha5@hotmail.com>
2024-03-23 03:37:27 +0200

    add Sex enum class; add two new packages feature/account_setup/fillprofile/views and feature/account_setup/fillprofile/status; refactor FillYourProfileFragment namespace. It now resides in the views package

---
+++ app/src/main/java/com/example/movio/feature/account_setup/fill_profile/status/Sex.kt
@@ -0,0 +1,6 @@
+package com.example.movio.feature.account_setup.fill_profile.status
+
+enum class Sex(val sexAsString: String) {
+    MALE("Male"),
+    FEMALE("Female")
+}
--- app/src/main/java/com/example/movio/feature/account_setup/fill_profile/views/FillYourProfileFragment.kt
+++ app/src/main/java/com/example/movio/feature/account_setup/fill_profile/views/FillYourProfileFragment.kt
@@ -1,5 +1,6 @@
-package com.example.movio.feature.account_setup.fill_profile
+package com.example.movio.feature.account_setup.fill_profile.views
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,30 +8,28 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.example.movio.databinding.FragmentFillYourProfileBinding
+import com.example.movio.feature.account_setup.fill_profile.status.Sex
 
 class FillYourProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentFillYourProfileBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = FragmentFillYourProfileBinding.inflate(inflater,container,false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initSexDropDownMenuUIState()
+    }
 
-        val items = arrayOf("Male","Female")
-        val adapter = ArrayAdapter(requireContext(),android.R.layout.simple_dropdown_item_1line,items)
-        binding.actSex.setAdapter(adapter)
+    private fun initSexDropDownMenuUIState(){
+        val items = arrayOf(Sex.MALE.sexAsString, Sex.FEMALE.sexAsString)
+        binding.actSex.setAdapter(ArrayAdapter(requireContext(), R.layout.simple_dropdown_item_1line,items))
+        binding.actSex.setText(items[0],false)
     }
 }
