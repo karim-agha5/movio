d3b05889493add1d531fae80c55f3ddd47bc99c7
karim <karim_agha5@hotmail.com>
2024-03-28 17:51:55 +0200

    chore: SplashFragment cleanup

--- app/src/main/java/com/example/movio/feature/splash/views/SplashFragment.kt
+++ app/src/main/java/com/example/movio/feature/splash/views/SplashFragment.kt
@@ -7,23 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.LifecycleEventObserver
-import com.example.movio.MainActivity
 import com.example.movio.core.common.BaseFragment
-import com.example.movio.core.common.BaseViewModel
-import com.example.movio.core.navigation.RootCoordinator
 import com.example.movio.databinding.FragmentSplashBinding
 import com.example.movio.feature.common.actions.AuthenticationActions
 import com.example.movio.feature.common.status.UserAuthenticationStatus
-import com.example.movio.feature.splash.viewmodel.SplashViewModel
 
 class SplashFragment : BaseFragment<FragmentSplashBinding,Nothing,AuthenticationActions, UserAuthenticationStatus>(SplashFragment::class.java),LifecycleEventObserver {
-
-    private lateinit var splashViewModel: BaseViewModel<Nothing,AuthenticationActions, UserAuthenticationStatus>
-
-    /*override fun onAttach(context: Context) {
-        super.onAttach(context)
-        activity?.lifecycle?.addObserver(this)
-    }*/
 
     override fun inflateBinding(
         inflater: LayoutInflater,
@@ -43,24 +32,6 @@ class SplashFragment : BaseFragment<FragmentSplashBinding,Nothing,Authentication
     * Navigates to either the AuthenticationFragment or to the HomeFragment
     * */
     private fun navigateFromSplash(){
-        //splashViewModel.postAction(null,AuthenticationActions.FromSplashScreen)
         viewModel.postAction(null,AuthenticationActions.FromSplashScreen)
     }
-
-    /**
-     * Necessary for the [SplashViewModel] instantiation.
-     * The instantiation of the [SplashViewModel] requires the [MainActivity] onCreate() lifecycle
-     * callback to be called first so that it initializes the [RootCoordinator] state correctly.
-     * Therefore, an observation on the [MainActivity] lifecycle is necessary.
-     * The observation on the [MainActivity] lifecycle is registered in the [onAttach] lifecycle callback.
-    * */
-    /*override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
-        if (event == Lifecycle.Event.ON_CREATE) {
-            val vm by ViewModelDelegate<Nothing, AuthenticationActions, UserAuthenticationStatus>(
-                movioApplication,
-                this::class.java
-            )
-            splashViewModel = vm
-        }
-    }*/
 }
