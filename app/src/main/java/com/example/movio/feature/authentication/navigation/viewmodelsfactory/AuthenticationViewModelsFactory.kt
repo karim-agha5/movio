22e90735d925afaec11b4c20c804b004c567fb95
karim <karim_agha5@hotmail.com>
2024-03-31 02:49:23 +0200

    feat: add createFillYourProfileViewModel in AuthenticationViewModelsFactory to return a viewmodel instance for FillYourProfile fragment

--- app/src/main/java/com/example/movio/feature/authentication/navigation/viewmodelsfactory/AuthenticationViewModelsFactory.kt
+++ app/src/main/java/com/example/movio/feature/authentication/navigation/viewmodelsfactory/AuthenticationViewModelsFactory.kt
@@ -2,13 +2,14 @@ package com.example.movio.feature.authentication.navigation.viewmodelsfactory
 
 import android.app.Application
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.movio.core.MovioApplication
 import com.example.movio.core.common.Action
 import com.example.movio.core.common.BaseViewModel
 import com.example.movio.core.common.Data
 import com.example.movio.core.common.Status
-import com.example.movio.feature.authentication.helpers.AuthenticationHelper
+import com.example.movio.feature.account_setup.fill_profile.viewmodel.FillYourProfileViewModel
+import com.example.movio.feature.account_setup.fill_profile.viewmodel.FillYourProfileViewModelFactory
+import com.example.movio.feature.account_setup.fill_profile.views.FillYourProfileFragment
 import com.example.movio.feature.authentication.services.EmailAndPasswordAuthenticationService
 import com.example.movio.feature.authentication.signin.viewmodel.SignInViewModel
 import com.example.movio.feature.authentication.signin.viewmodel.SignInViewModelFactory
@@ -19,11 +20,9 @@ import com.example.movio.feature.authentication.signup.views.SignupFragment
 import com.example.movio.feature.authentication.viewmodels.AuthenticationViewModel
 import com.example.movio.feature.authentication.viewmodels.AuthenticationViewModelFactory
 import com.example.movio.feature.authentication.views.AuthenticationFragment
-import com.example.movio.feature.common.helpers.UserManager
 import com.example.movio.feature.splash.viewmodel.SplashViewModel
 import com.example.movio.feature.splash.viewmodel.SplashViewModelFactory
 import com.example.movio.feature.splash.views.SplashFragment
-import com.google.firebase.auth.FirebaseAuth
 
 // TODO refactor this class to inherit AndroidViewModelFactory
 class AuthenticationViewModelsFactory(
@@ -75,12 +74,15 @@ class AuthenticationViewModelsFactory(
         return factory.create(AuthenticationViewModel::class.java)
     }
 
+    private fun createFillYourProfileViewModel() = FillYourProfileViewModelFactory(application).create(FillYourProfileViewModel::class.java)
+
      fun createViewModel(cls: Class<out Fragment>) : BaseViewModel<out Data,out Action, out Status> {
         return when(cls){
             SplashFragment::class.java          -> createSplashViewModel()
             SignupFragment::class.java          -> createSignupViewModel()
             SignInFragment::class.java          -> createSignInViewModel()
             AuthenticationFragment::class.java  -> createAuthenticationViewModel()
+            FillYourProfileFragment::class.java -> createFillYourProfileViewModel()
             else                                -> throw IllegalArgumentException()
         }
     }
