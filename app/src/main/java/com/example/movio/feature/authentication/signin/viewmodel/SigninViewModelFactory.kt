29a31c9d4a9ce299abbcf436c7404157d4951d75
karim <karim_agha5@hotmail.com>
2024-04-03 19:38:06 +0200

    chore: SignInViewModelFactory cleanup

--- app/src/main/java/com/example/movio/feature/authentication/signin/viewmodel/SigninViewModelFactory.kt
+++ app/src/main/java/com/example/movio/feature/authentication/signin/viewmodel/SigninViewModelFactory.kt
@@ -3,27 +3,15 @@ package com.example.movio.feature.authentication.signin.viewmodel
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.movio.feature.authentication.helpers.AuthenticationHelper
-import com.example.movio.feature.authentication.services.EmailAndPasswordAuthenticationService
-import com.example.movio.feature.authentication.services.GoogleSignInService
-import com.example.movio.feature.authentication.services.TwitterAuthenticationService
 import com.example.movio.feature.common.data_access.IAuthenticationRepository
 
 class SignInViewModelFactory(
-    private val emailAndPasswordAuthenticationService: EmailAndPasswordAuthenticationService,
-    //private val googleSignInService: GoogleSignInService,
-    //private val twitterAuthenticationService: TwitterAuthenticationService,
-    //private val authenticationHelper: AuthenticationHelper,
     private val application: Application,
     private val authenticationRepository: IAuthenticationRepository
 ) : ViewModelProvider.Factory{
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return SignInViewModel(
-            //emailAndPasswordAuthenticationService,
-            //googleSignInService,
-            //twitterAuthenticationService,
-            //authenticationHelper,
             application,
             authenticationRepository
         ) as T
