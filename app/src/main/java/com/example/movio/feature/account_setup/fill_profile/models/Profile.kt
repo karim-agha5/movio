64fa4e7bf1ac5f5753b5099a204ba1511158faa3
karim <karim_agha5@hotmail.com>
2024-04-06 23:19:25 +0200

    feat: FillYourProfile now uses data binding

--- app/src/main/java/com/example/movio/feature/account_setup/fill_profile/models/Profile.kt
+++ app/src/main/java/com/example/movio/feature/account_setup/fill_profile/models/Profile.kt
@@ -4,8 +4,8 @@ import com.example.movio.core.common.Data
 import com.example.movio.feature.account_setup.fill_profile.status.Sex
 
 data class Profile(
-    val fullName: String,
-    val nameTag: String,
+    val fullName: String = "",
+    val nameTag: String = "",
     val sex: Sex = Sex.MALE,
-    val phoneNumber: String
+    val phoneNumber: String = ""
 ) : Data
