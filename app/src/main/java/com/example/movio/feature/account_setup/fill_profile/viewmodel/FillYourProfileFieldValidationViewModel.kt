2b7c8129bc48256e103da5be1537e6325358d02b
karim <karim_agha5@hotmail.com>
2024-04-09 14:00:24 +0200

    feat: add FillYourProfileFieldValidationViewModelFactory

--- app/src/main/java/com/example/movio/feature/account_setup/fill_profile/viewmodel/FillYourProfileFieldValidationViewModel.kt
+++ app/src/main/java/com/example/movio/feature/account_setup/fill_profile/viewmodel/FillYourProfileFieldValidationViewModel.kt
@@ -1,9 +1,10 @@
 package com.example.movio.feature.account_setup.fill_profile.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.movio.feature.account_setup.fill_profile.use_cases.ValidateFullName
 
 class FillYourProfileFieldValidationViewModel(
-
+    private val validateFullName: ValidateFullName
 ) : ViewModel(){
 
 }
---
+++ app/src/main/java/com/example/movio/feature/account_setup/fill_profile/viewmodel/FillYourProfileFieldValidationViewModelFactory.kt
@@ -0,0 +1,13 @@
+package com.example.movio.feature.account_setup.fill_profile.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.movio.feature.account_setup.fill_profile.use_cases.ValidateFullName
+
+class FillYourProfileFieldValidationViewModelFactory(
+    private val validateFullName: ValidateFullName
+) : ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return FillYourProfileFieldValidationViewModel(validateFullName) as T
+    }
+}
